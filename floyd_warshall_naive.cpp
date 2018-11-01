871495a51d7ec805113371b75e416195fc1052e5
Raynold <raynold.ng24@gmail.com>
2018-11-01 13:09:30 +0800

    First commit

---
+++ floyd_warshall_naive.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define db(x) cerr << #x << "=" << x << endl
+#define db2(x, y) cerr << #x << "=" << x << "," << #y << "=" << y << endl
+#define db3(x, y, z) cerr << #x << "=" << x << "," << #y << "=" << y << "," << #z << "=" << z << endl
+
+#define INF 1e8
+#define TEST_V 10
+
+int V;
+
+int main() {
+	scanf("%d", &V);
+	// V = 10;
+	int **dist = new int*[V];
+	for(int i = 0; i < V; i++) dist[i] = new int[V];
+
+	for(int i = 0; i < V; i++)
+		for(int j = 0; j < V; j++)
+			dist[i][j] = INF;
+	
+	int u, v;
+	
+	while(scanf(" %d %d", &u, &v)) {
+		dist[u][v] = dist[v][u] = 1;
+	}
+
+	for(int k = 0; k < V; k++) {
+		if(k % 100) db(k);
+		for(int i = 0; i < V; i++) {
+			for(int j = 0; j < V; j++) {
+				dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j]);
+			}
+		}
+	}
+
+	// print out the first 10
+	for(int i = 0; i < TEST_V; i++) {
+		for(int j = 0; j < TEST_V; j++) {
+			printf("%d\t", dist[i][j]);
+		}
+		printf("\n");
+	}
+
+}
